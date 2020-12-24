26034728366d5ba44e10b36e82c74d3445a3fd6d
solymansa <solyman4444@gmail.com>
2020-12-24 23:18:05 +0300

    first commit

--- site3/js/first.js
+++ site3/js/first.js
@@ -1,6 +1,6 @@
 
 
-function change() { 
+function change() {
 
     var input1 = document.getElementById("input1").value;
 
@@ -10,51 +10,52 @@ function change() {
     var checkBox = document.getElementById("check");
 
     console.log(word.toUpperCase());
-    
-    
+
+
     word = word.trim();
     changeword = changeword.trim();
 
     var input1L = input1.split("\n");
 
-    for(var i = 0 ; i < input1L.length ; i++) { 
-        
+    for (var i = 0; i < input1L.length; i++) {
+
         input1L[i] = " " + input1L[i] + " ";
     }
 
     input1 = input1L.join("\n");
 
 
-    
+
     var input1A = input1.split(" ");
     var count = 0;
-    
-    if(checkBox.checked == true)  { 
-        for(var i = 0 ; i < input1A.length ; i++) { 
-            
-            if(input1A[i] == word) { 
+
+    if (checkBox.checked == true) {
+        for (var i = 0; i < input1A.length; i++) {
+
+            if (input1A[i] == word) {
                 input1A[i] = changeword;
                 count++;
             }
-            
+
         }
-    } else { 
-        for(var i = 0 ; i < input1A.length ; i++) { 
-            
-            if(input1A[i].toUpperCase() == word.toUpperCase()) { 
+    } else {
+        for (var i = 0; i < input1A.length; i++) {
+
+            if (input1A[i].toUpperCase() == word.toUpperCase()) {
                 input1A[i] = changeword;
                 count++;
             }
-            
+
         }
     }
 
 
-    if(word != "")
-    document.getElementById("counter").innerHTML = "The word occurs : "+ count + " Times";
+    if (word != "")
+        document.getElementById("counter").innerHTML = "The word occurs : " + count + " Times";
 
     document.getElementById("input2").value = input1A.join(" ");
-    
 
-   
+
+
 }
+
